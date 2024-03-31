cc5bda20060882c2fbd50675ef13261c9782cd4a
Priyansh <shahpriyansh598@gmail.com>
2024-03-30 19:26:23 -0700

    Updated styles.css and moreDetails.js, and modified details.ejs and index.ejs

--- public/js/moreDetails.js
+++ public/js/moreDetails.js
@@ -8,12 +8,17 @@ if (Array.isArray(weatherData.data)) {
 
     if (dataForDate) {
         document.getElementById('date').textContent = dataForDate.date;
-        document.getElementById('avgTemp').textContent = dataForDate.day.avgtemp_c;
-        document.getElementById('maxTemp').textContent = dataForDate.day.maxtemp_c;
-        document.getElementById('minTemp').textContent = dataForDate.day.mintemp_c;
+        document.getElementById('avgTemp').textContent = dataForDate.day.avgtemp_f;
+        document.getElementById('maxTemp').textContent = dataForDate.day.maxtemp_f;
+        document.getElementById('minTemp').textContent = dataForDate.day.mintemp_f;
         document.getElementById('humidity').textContent = dataForDate.day.avghumidity;
+        document.getElementById('maxWind').textContent = dataForDate.day.maxwind_mph;
+        document.getElementById('totalPrecip').textContent = dataForDate.day.totalprecip_mm;
+        document.getElementById('uvIndex').textContent = dataForDate.day.uv;
         document.getElementById('sunrise').textContent = dataForDate.astro.sunrise;
         document.getElementById('sunset').textContent = dataForDate.astro.sunset;
+        document.getElementById('conditionIcon').src = dataForDate.day.condition.icon;
+        document.getElementById('conditionText').textContent = dataForDate.day.condition.text;
     } else {
         console.error('No data found for the specified date.');
     }
